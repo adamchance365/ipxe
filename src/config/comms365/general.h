bb8e15965aed410772e4ff6322d15d52bf8f7327
Adam Chance <adam.chance@comms365.com>
2018-11-14 11:26:05 +0000

    Disable HTTPS

--- src/config/comms365/general.h
+++ src/config/comms365/general.h
@@ -4,6 +4,4 @@
 #undef BANNER_TIMEOUT
 #define BANNER_TIMEOUT	0
 
-#define	DOWNLOAD_PROTO_HTTPS	/* Secure Hypertext Transfer Protocol */
-
 #define	IMAGE_COMBOOT		/* SYSLINUX COMBOOT image support */
